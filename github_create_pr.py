c165047c2f27df0205a5d29fdc9fc2767b387437
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-09-23 12:04:32 +0300

    Consistent intendation

--- github_create_pr.py
+++ github_create_pr.py
@@ -1,24 +1,24 @@
 import sublime, sublime_plugin, webbrowser
 try:
-  from .github import *
+    from .github import *
 except ValueError:
-  from github import *
+    from github import *
 
 
 class GithubCreatePrCommand(GithubWindowCommand):
-  @with_repo
-  def run(self, repo):
-    self.remotes      = repo.remotes
-    self.remote_names = list(self.remotes.keys())
-    self.repo         = repo
-    self.window.show_quick_panel(self.remote_names, self.remote_selected)
+    @with_repo
+    def run(self, repo):
+        self.remotes      = repo.remotes
+        self.remote_names = list(self.remotes.keys())
+        self.repo         = repo
+        self.window.show_quick_panel(self.remote_names, self.remote_selected)
 
-  def remote_selected(self, index):
-    url = self.remotes[self.remote_names[index]]
-    remote = re.search(":(\w+)\/", url).group(1)
-    webbrowser.open_new_tab(self.generate_url(remote))
+    def remote_selected(self, index):
+        url = self.remotes[self.remote_names[index]]
+        remote = re.search(":(\w+)\/", url).group(1)
+        webbrowser.open_new_tab(self.generate_url(remote))
 
-  def generate_url(self, remote):
-    branch = self.repo.branch
-    interpolations = (self.repo.info['web_uri'], remote, branch, branch)
-    return "https://%s/compare/%s:%s...%s" % interpolations
+    def generate_url(self, remote):
+        branch = self.repo.branch
+        interpolations = (self.repo.info['web_uri'], remote, branch, branch)
+        return "https://%s/compare/%s:%s...%s" % interpolations
