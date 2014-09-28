52b943145259d28c54e64910baf90b200e70cf8c
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-09-28 19:16:31 +0300

    Use special github link to create the PR, no remote magic needed

--- github_create_pr.py
+++ github_create_pr.py
@@ -8,17 +8,8 @@ except ValueError:
 class GithubCreatePrCommand(GithubWindowCommand):
     @with_repo
     def run(self, repo):
-        self.remotes      = repo.remotes
-        self.remote_names = list(self.remotes.keys())
-        self.repo         = repo
-        self.window.show_quick_panel(self.remote_names, self.remote_selected)
+        webbrowser.open_new_tab(self.generate_url(repo))
 
-    def remote_selected(self, index):
-        url = self.remotes[self.remote_names[index]]
-        remote = re.search(":(\w+)\/", url).group(1)
-        webbrowser.open_new_tab(self.generate_url(remote))
-
-    def generate_url(self, remote):
-        branch = self.repo.branch
-        interpolations = (self.repo.info['web_uri'], remote, branch, branch)
-        return "https://%s/compare/%s:%s...%s" % interpolations
+    def generate_url(self, repo):
+        interpolations = (repo.info['web_uri'], repo.branch)
+        return "https://%s/compare/%s?expand=1" % interpolations
