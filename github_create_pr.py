60f7d6cb849050ab5b9b3e7c2422c902e4e61762
Andrei Lisnic <andrei.lisnic@gmail.com>
2014-09-28 21:07:52 +0300

    Move the url generator to where it belongs

--- github_create_pr.py
+++ github_create_pr.py
@@ -8,8 +8,4 @@ except ValueError:
 class GithubCreatePrCommand(GithubWindowCommand):
     @with_repo
     def run(self, repo):
-        webbrowser.open_new_tab(self.generate_url(repo))
-
-    def generate_url(self, repo):
-        interpolations = (repo.info['web_uri'], repo.branch)
-        return "https://%s/compare/%s?expand=1" % interpolations
+        webbrowser.open_new_tab(git_compare_url(repo.info['web_uri'], repo.branch))
